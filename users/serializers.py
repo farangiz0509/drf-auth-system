0bec5251574b79125368d50769255d007bdfeb5c
djumanov <djumanovdev@gmail.com>
2026-02-07 09:20:40 +0500

    register

---
+++ users/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    confirm = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'confirm', 'first_name', 'last_name', 'email', 'phone_number')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm']:
+            raise ValidationError('password and confirm must be the same.')
+        
+        attrs.pop('confirm')
+        attrs['password'] = make_password(attrs['password'])
+
+        return super().validate(attrs)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    
+    def post(self, request: Request) -> Response:
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            return Response(status=status.HTTP_201_CREATED)
